92b7e1f48e2d20743086520e845740c3340a4c57
Depplin.net <76627174+depplinnet@users.noreply.github.com>
2025-05-03 19:16:53 +0000

    Funcionando

--- fggcleaning.app/src/app/components/btn-whatsapp/btn-whatsapp.component.ts
+++ fggcleaning.app/src/app/components/btn-whatsapp/btn-whatsapp.component.ts
@@ -1,5 +1,6 @@
 import { ChangeDetectionStrategy, Component, inject, model, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { MatBottomSheet, MatBottomSheetModule, MatBottomSheetRef } from '@angular/material/bottom-sheet';
 import { MatButtonModule } from '@angular/material/button';
 import { 
   MAT_DIALOG_DATA, // Se usa en lialog
@@ -11,7 +12,9 @@ import {
   MatDialogTitle 
 } from '@angular/material/dialog'; // Solo necesitamos MatDialog aquí
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
 
 export interface DialogData {
   message: string;
@@ -21,22 +24,61 @@ export interface DialogData {
 @Component({
   selector: 'app-btn-whatsapp',
   standalone: true, // Si es un componente standalone
-  imports: [ 
-    MatFormFieldModule, 
-    MatInputModule, FormsModule, 
-    MatButtonModule ],
+  imports: [ MatFormFieldModule, MatInputModule, FormsModule, MatButtonModule, MatIconModule ],
   changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './btn-whatsapp.component.html',
   styleUrl: './btn-whatsapp.component.css'
 })
 export class BtnWhatsappComponent {
 
-  public pregunta = "What's your name ?";
+  public nombreBoton = 'Contact';
+  public whatsappNumber = signal('573053000000');
 
-  readonly message = signal('');
-  readonly name = model('');
+  private _bottomSheet = inject(MatBottomSheet);
+
+  openBottomSheet(): void {
+    this._bottomSheet.open(Sheet);
+  }
+
+
+
+
+  
+
+  
+}
+
+
+
+@Component({
+  selector: 'sheet',
+  templateUrl: 'sheet.html',
+  styleUrl: 'btn-whatsapp.component.css',
+  imports: [MatListModule, MatFormFieldModule, MatInputModule, FormsModule, MatButtonModule
+  ],
+})
+export class Sheet {
+
+  private _bottomSheetRef =
+    inject<MatBottomSheetRef<Sheet>>(MatBottomSheetRef);
+
+  openLink(event: MouseEvent): void {
+    this._bottomSheetRef.dismiss();
+    event.preventDefault();
+  }
+
+  public preguntaName = "What's your name ?";
+  public preguntaPhone = "What's your Phone ?";
+  public preguntaEmail = "What's your Email ?";
+
+  public message = signal('');
+  public name = model('');
+  public phone = model('');
+  public email = model('');
+
   readonly dialog = inject(MatDialog);
 
+  
   openDialog(): void {
     const dialogRef = this.dialog.open(Dialog, {
       width: '90%',
@@ -51,10 +93,19 @@ export class BtnWhatsappComponent {
       console.log('The dialog was closed');
       if (result !== undefined) {
         this.message.set(result);
+        this.name.set(result.name);
+        this.phone.set(result.phone);
+        this.email.set(result.email);
+        console.log('Dialog result:', result);
       }
     });
   }
+
+
+
+
 }
+
 
 
 
@@ -68,6 +119,7 @@ export class BtnWhatsappComponent {
   templateUrl: 'dialog.html',
   styleUrl: 'btn-whatsapp.component.css',
   imports: [
+    MatBottomSheetModule,
     MatFormFieldModule,
     MatInputModule,
     FormsModule,
